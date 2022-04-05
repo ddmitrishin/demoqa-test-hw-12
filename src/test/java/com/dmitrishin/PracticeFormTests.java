139d61bd36e8d7b32a1e4390726c483467467870
ddmitrishin <dmitrishin52@gmail.com>
2022-04-05 23:28:52 +0500

    Added a new class that tests the practical form and one image

---
+++ src/test/java/com/dmitrishin/PracticeFormTests.java
@@ -0,0 +1,77 @@
+package com.dmitrishin;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillPracticeFormTest() {
+
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        //Input parameters
+        String firstName = "Dima";
+        String lastName = "Dmitrishin";
+        String email = "test@example.com";
+        String gender = "Male";
+        String phone = "9199960164";
+        String subject = "Physics";
+        String imagePath = "img/Avatar.jpg";
+        String address = "Komsa street 11, 15 apt.";
+        String state = "Haryana";
+        String city = "Karnal";
+
+        //Fill form
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(phone);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1992");
+        $(byText("16")).click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath(imagePath);
+        $("#currentAddress").setValue(address);
+        $("#state").scrollTo();
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").click();
+
+        //Form validation
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
+                text("Label Value"),
+                text("Student Name " + firstName + " " + lastName),
+                text("Student Email " + email),
+                text("Gender " + gender),
+                text("Mobile " + phone),
+                text("Date of Birth 16 June,1992"),
+                text("Subjects " + subject),
+                text("Hobbies Sports"),
+                text("Picture Avatar.jpg"),
+                text("Address " + address),
+                text("State and City " + state + " " + city));
+
+        $("#closeLargeModal").click();
+    }
+}
